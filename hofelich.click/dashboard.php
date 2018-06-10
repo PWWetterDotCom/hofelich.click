3baf166bb6ae6f349b06c2e5323138fa0d36cf45
PWWetterDotCom <40126341+PWWetterDotCom@users.noreply.github.com>
2018-06-10 14:37:51 +0200

    Add files via upload

---
+++ hofelich.click/dashboard.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <!-- ######################################## BOOTSTRAP TEIL 1 ######################################## -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Die 3 Meta-Tags oben *müssen* zuerst im head stehen; jeglicher sonstiger head-Inhalt muss *nach* diesen Tags kommen -->
+    
+    <link rel="icon" type="image/png" href="../favicon.png" sizes="64x64">
+    
+    <!-- Bootstrap -->
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Unterstützung für Media Queries und HTML5-Elemente in IE8 über HTML5 shim und Respond.js -->
+    <!-- ACHTUNG: Respond.js funktioniert nicht, wenn du die Seite über file:// aufrufst -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- ######################################## ENDE BOOTSTRAP TEIL 1######################################## -->
+    <title>Hofelich Dasboard</title>
+  </head>
+  <body>
+<!-- DEVELOPE FROM HERE-->
+<br><br><br>
+<div class="container text-center">
+ <div class="row">
+  <div class="col-sm-offset-3 col-sm-2">
+    <a href="../amazon-gratis-produkte">Amazon gratis Produkte</a>
+    <h6>Status: Alpha</h6>
+  </div>
+   <div class="col-sm-2">
+    <a href="../musikwolke7">Musikwolke 7</a>
+     <h6>Status: nicht begonnen</h6>
+  </div>
+  <div class="col-sm-2">
+    <a href="http://playvode.de">Ghoster</a>
+    <h6>Status: Alpha</h6>
+  </div>
+ </div>
+ <br><br><br>
+ <div class="row">
+  <div class="col-sm-offset-3 col-sm-2">
+    <a href="../login-test">Login-Test</a>
+    <h6>Status: <a href="https://de.wikihow.com/Ein-sicheres-Login-Skript-mit-PHP-und-MySQL-erstellen">Code übertragen</a></h6>
+  </div>
+   <div class="col-sm-2">
+    <a href="http://hofelich.click/phpmyadmin">Zur MySQL Datenbank</a>
+  </div>
+  <div class="col-sm-2">
+    <a href="#">Comming soon...</a>
+  </div>
+ </div>
+</div>
+
+
+<!-- UNTIL HERE-->
+    <!-- ######################################## BOOTSTRAP TEIL 2 ######################################## -->
+    <!-- jQuery (wird für Bootstrap JavaScript-Plugins benötigt) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Binde alle kompilierten Plugins zusammen ein (wie hier unten) oder such dir einzelne Dateien nach Bedarf aus -->
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+    <!-- ######################################## ENDE BOOTSTRAP TEIL 2 ######################################## -->
+  </body>
+</html>
